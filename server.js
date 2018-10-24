c491f1fc874c74718000b4c64a48b34d14444a20
Andi Bašić <andi.basic10@gmail.com>
2018-10-24 19:26:04 +0200

    security

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
-
-
 const db = knex({
 	client: 'pg',
 	connection: {
@@ -41,6 +38,8 @@ app.post('/signin', (req,res) => { signin.handleSignin(req, res, db, bcrypt)} ) 
 app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt)} )
 app .get('/profile/:id',(req, res) => { profile.handleProfileGet(req, res, db)} )
 app.put('/image', (req,res)=> { image.handleImage(req, res, db)} )
+app.post('/imageurl', (req,res)=> { image.handleApiCall(req, res)} )
+
 
 app.listen(3000, ()=>{
 	console.log('app is running on port 3000')
